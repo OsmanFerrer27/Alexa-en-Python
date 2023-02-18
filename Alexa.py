7c8b2279157115f6dffebc511535257e32a9c788
OsmanFerrer27 <91578666+OsmanFerrer27@users.noreply.github.com>
2023-02-18 14:21:55 -0400

    Add files via upload

--- Alexa.py
+++ Alexa.py
@@ -4,52 +4,59 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+engine.runAndWait()
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-
+    
+    
 def take_command():
     try:
         with sr.Microphone() as source:
             print('listening...')
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
-            print(command)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', 'ggggttftvtvtf')
+                print(command)
     except:
         pass
     return command
-
+    
 def run_alexa():
     command = take_command()
     print(command)
     if 'play' in command:
         song = command.replace('play', '')
-        talk('playing ' + song)
+        talk('playing' + song)
         pywhatkit.playonyt(song)
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
+        talk('The current time is ' + time)
+        print(time)
     elif 'who is' in command:
         person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
     elif 'joke' in command:
         talk(pyjokes.get_joke())
+    elif 'hello' in command:
+        talk('Hi!')
+    elif 'how are you doing' in command:
+        talk('I am doing good! Thanks for asking!')
+    elif 'bye' in command:
+        quit
     else:
-        talk('please say the command again')
+        talk('Please say the command again.')
+        
 
 while True:
     run_alexa()
